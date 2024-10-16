499c511c457f61cb27896a8e76dd723202e8a92c
Neha Vinamra <vinamraneha@gmail.com>
2024-10-16 14:21:19 -0400

    icons

--- MusicPlayer/app/(tabs)/_layout.tsx
+++ MusicPlayer/app/(tabs)/_layout.tsx
@@ -1,10 +1,34 @@
 import { Tabs} from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons'
 export default function TabLayout() {
   return (
-    <Tabs >
-      <Tabs.Screen name="index" options={{ title: 'Home' }}  />
-      <Tabs.Screen name="liked" options={{ title: 'Liked' }} />
-      <Tabs.Screen name="mymusic" options={{ title: 'My Music' }} />
+    <Tabs  screenOptions={{
+      tabBarActiveTintColor: '#ffd33d',
+    headerStyle: {
+      backgroundColor: '#25292e',
+    },
+    headerShadowVisible: false,
+    headerTintColor: '#fff',
+    tabBarStyle: {
+    backgroundColor: '#25292e',
+    },
+    }}>
+      <Tabs.Screen name="index" options={{ 
+        title: 'Home', 
+        tabBarIcon: ({ color}) => (
+            <Ionicons name= "home-outline" color={color} size={24} />
+          ), }}  />
+      <Tabs.Screen name="liked" options={{
+        title: 'Liked',
+        tabBarIcon: ({ color}) => (
+          <Ionicons name= "heart-outline" color={color} size={24} />
+        ),
+         }} />
+      <Tabs.Screen name="mymusic" options={{ 
+        title: 'My Music',
+        tabBarIcon: ({ color}) => (
+          <Ionicons name= "musical-notes-outline" color={color} size={24} />
+        ), }} />
     </Tabs>
 
   );
